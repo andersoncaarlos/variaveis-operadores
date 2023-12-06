c5d65ad98528084882afd25cfd28599d5979e7ea
Anderson <ander_son12@hotmail.com>
2023-12-06 12:55:31 -0300

    Exercicio media

---
+++ src/ExercicioMedia.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioMedia {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Digite a primeira nota: ");
+		float nota1 = sc.nextFloat();
+		
+		System.out.print("Digite a segunda nota: ");
+		float nota2 = sc.nextFloat();
+		
+		System.out.print("Digite a terceira nota: ");
+		float nota3 = sc.nextFloat();
+		
+		System.out.print("Digite a quarta nota: ");
+		float nota4 = sc.nextFloat();
+		
+		float media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println();
+		System.out.println("Média final " + String.format("%.1f", media));
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
